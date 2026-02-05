1a0ae3f95f49f577dd202c40d81a373af1e7e94d
Your Name <you@example.com>
2026-02-05 11:52:47 +0530

    msg

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,25 +1,51 @@
-const http=require('http');
+// const http=require('http');
 
 
-const myserver=http.createServer((req,res)=>{
-    // console.log('serever 1');
-    // res.end("this is my first server");
-    if(req.url=='/'){
-        res.end(`abes engineering college`);
+// const myserver=http.createServer((req,res)=>{
+//     // console.log('serever 1');
+//     // res.end("this is my first server");
+//     if(req.url=='/'){
+//         res.end(`abes engineering college`);
+
         
-        
 
-    }
-    else if(req.url=='/about'){
-        res.end("we are studdent");
-    }
-    else if(req.url=='/class'){
-        res.end("CE");
-    }
-    else
-    {
-        res.end("error");
-    }
+//     }
+//     else if(req.url=='/about'){
+//         res.end("we are studdent");
+//     }
+//     else if(req.url=='/class'){
+//         res.end("CE");
+//     }
+//     else
+//     {
+//         res.end("error");
+//     }
 
-})
-myserver.listen(8000,()=>console.log("server is run"))
+// })
+// myserver.listen(8000,()=>console.log("server is run"))
+// const fs=require('fs');
+// fs.cpSync("IT-A.txt","CE-A.txt");
+
+
+
+
+
+
+
+
+const fs=require('fs')
+// fs.writeFile("./IT-A.txt","we are student of it-a",()=>{ })
+
+    fs.readFile("./IT-A","utf-8",(err,result)=>{
+        if(err){
+            console.log("error",err);
+        }
+        else{
+            console.log(result);
+        }
+    })
+
+// const fs=require('fs')
+// // fs.writeFileSync("abes.txt","abes provide btech and mtech")
+// const result=fs.readFileSync("./cse-a.txt","utf-8")
+// console.log(result);
